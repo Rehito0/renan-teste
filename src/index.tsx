4d60799a260d163344c89c799d97c9119f7a6cc4
Renan <72417996+Rehito0@users.noreply.github.com>
2025-03-10 13:20:18 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@
           import { tools } from './tools';
 
           // ---------- set Caps Inputs
-          const currRoute = 'sc1';
+          const currRoute = 'sc3';
 
           let args: any = [];
 
