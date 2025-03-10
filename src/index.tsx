96ef5c5dc685bad2f6197d90519502300b62ad19
Renan <72417996+Rehito0@users.noreply.github.com>
2025-03-10 13:33:59 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -205,20 +205,26 @@
 
           styles:[`{ backgroundColor: "white", flex: 1, width: "100%", heigh: "100%" }`],
 
-          screenElements:[() => {
-                  const textStyle = {
-                    fontSize: 20,
-                    color: '#fff2',
-                    textAlign:'center',
-                    maxWidth: 200,
-                    // maxWidth: '200px'<= #ATTENTION: Native ERROR! No string!
-                  };
+          screenElements:[(...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
 
-                  return (
-                    <RN.Text style={textStyle}>
-                      {'Adicione Elementos nessa tela!'}
-                    </RN.Text>);
-                }],
+          arrStyles: [
+            {
+    color: 'red',
+    fontSize: 16,
+    fontWeight: 'bold', 
+}
+          ],
+
+          children: [
+            `ERRO`
+          ],
+
+          args,
+
+        }}/>],
 
           functions:[()=>{}],
 
