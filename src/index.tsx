7fcd4f4a9d6c8cfb1e4e7b7e06e9c83538a8ac91
Renan <72417996+Rehito0@users.noreply.github.com>
2025-03-14 10:28:16 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -92,7 +92,21 @@
               }
               ],
 
-            functions:[()=>{}],            childrenItems:[() =><></>],
+            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.IptTxtEdit pass={{
+          propsArray: [{}],
+
+          stylesArray: [{
+                color: 'black',
+                fontSize: 14,
+                // fontSize: '20px',<= #ATTENTION: Native ERROR! No string!
+              }],
+
+          editPath: [" "],
+
+          funcsArray: [() => {}],
+
+          args,
+        }}/>],
 
             args,
           }}/>
